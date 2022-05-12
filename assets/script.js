e0c5aa308fa17fd32d59356fbfe06a9024ef08c2
dianaiuliacalin <92143901+dianaiuliacalin@users.noreply.github.com>
2022-05-12 00:33:06 -0700

    adding to pseudocode as js comments

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,22 @@
+// variables for:
+    // api key = "0a8d4d6d2e89417ea3d2045c8dfd04bb";
+    // (to push buttons into) searchHistoryEl = parse localstorage json data, get "searchedCities"
+        // OR empty array if there is no json data 
+        // render searchedCities into searchHistoryEl with data attr 
+    // styling for fiveDayForecast
+    
+    // document.querySelector() for 
+        // searchedCity 
+        // searchButton 
+
+        // currentWeather 
+        // fiveDayWeather 
+
+    // city = searchedCity.value.trim()
+
+// build url to get corresaponding weather icon from openweather api
+
+
 // Fetch geodata
      // .THEN fetch one call data
      // .THEN render weather to page 
@@ -31,3 +50,8 @@
 
 // from <button> CONTAINER element, listen for "click"
     // get the city from the button's data attr (when you print the buttons, print them with a data attr.)
+
+
+// RESOURCES: 
+// https://developer.mozilla.org/en-US/docs/Web/API/Response
+// https://openweathermap.org/api/one-call-api
