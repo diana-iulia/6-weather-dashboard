a327b49897c93b19f1e53676a7115404c89e4033
dianaiuliacalin <92143901+dianaiuliacalin@users.noreply.github.com>
2022-05-12 17:45:59 -0700

    building form submit, fx to get icon, and from anthony's demo

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,90 @@
-// variables for:
-    // api key = "0a8d4d6d2e89417ea3d2045c8dfd04bb";
-    // (to push buttons into) searchHistoryEl = parse localstorage json data, get "searchedCities"
-        // OR empty array if there is no json data 
-        // render searchedCities into searchHistoryEl with data attr 
-    // styling for fiveDayForecast
+const apiKey = "0a8d4d6d2e89417ea3d2045c8dfd04bb";
+var imperial = "&units=imperial";
+
+// search form (to add event listener to later)
+var searchedCityEl = document.querySelector("#searchedCityEl");
+// user input 
+var searchedCity = document.querySelector("#searchedCity");
+// do I need these (6,8) to be separate or can I combine them as `document.querySelector("#searchedCity").value.trim();` ?
+var city = searchedCity.value.trim();
+
+// to listen for click on search history buttons
+var searchButton = document.querySelectorAll(".searchButton");
+
+// grabs <div>s into which we will push our data
+var currentWeather = document.querySelector("#currentWeather");
+var fiveDayWeather = document.querySelector("#fiveDayWeather");
+
+// searchedCities is the data, searchHistory is the container it(data) goes into 
+var searchedCities = JSON.parse(localStorage.getItem("searchedCities")) || [];
+var searchHistory = document.querySelector("#searchHistory")
+
+// var styling for fiveDayForecast
+
+// how to handle form submit 
+var search = function(event, city) {
+    event.preventDefault();
     
-    // document.querySelector() for 
-        // searchedCity 
-        // searchButton 
+    if (city !== null) {
+        // console.log(city);
+        geoData(city);
+        
+        // render searchedCities into searchHistory (element) with data attr 
+        for ( i=0; i<searchHistory.length; i++ ) {
+            document.createElement(`<button href="#" class="btn btn-primary" aria-current="false" id="city" data-name="${searchedHistory[i]}">${searchedCity[i]}</button>`)
+            
+        }
+    }
 
-        // currentWeather 
-        // fiveDayWeather 
+},
 
-    // city = searchedCity.value.trim()
+// build url to get corresponding weather icon from openweather api
+var getIcon = function(iconCode) {
+    var iconURL = "https://openweathermap.org/img/wn/" + iconCode + "@2";
+    return iconURL;
+},
 
-// build url to get corresaponding weather icon from openweather api
-
-
-// Fetch geodata
-     // .THEN fetch one call data
-     // .THEN render weather to page 
-
-// fetch geodata (lat, lon)
-
+// Fetch geodata (lat, lon)
+var geoData = function( city ) {
     // q = name of city
-
-    // limit 5 (optional)
-
+    // limit results (optional)
     // appid = your custom API key
+    
+    var url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
+
+    // .THEN fetch one call data
+    fetch(url) 
+        .then(function(response){
+            return response.json();
+        })
+    // .THEN render weather to page (pass thru data, what we returned from first step)
+        .then(function(data) {
+            console.log(data);
+
+            getWeather();
+        })
+}
 
 // Fetch one call weather data
-
-    // lat
-
-    // lon
-
+var getWeather = function( lat, lon ) {
+    // lat, lon
     // appid
-
     // units = imperial
-
     // exclude = minutely, hourly
+    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}`;
+    searchedCity.innerHTML= "";
 
+// .THEN fetch one call data
+    fetch(url) 
+        .then(function(response){
+            return response.json();
+        })
+        // .THEN render weather to page (pass thru data, what we returned from first step)
+        .then(function(data) {
+            console.log(data);
+            // render information to page 
+        })
+}
 // from <form> el, listen for "submit" 
 // from <button> el, listen for "click"
 
